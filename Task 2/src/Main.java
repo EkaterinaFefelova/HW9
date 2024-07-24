65c69c9cbe7659c418475bf5e9b42250d9caf171
EkaterinaFefelova <fefelova.ekaterina@gmail.com>
2024-07-24 13:55:15 +0300

    HW9 task 2 completed

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
+        printTrueForEven(getInput());
         //Написать программу, в которой пользователь вводит целое число.
         //Если число четное, то печатаем true, если не четное, печатаем false
         //В методе main НЕ ДОПУСТИМО написание какого либо кода, кроме вызова методов.
         //Методы и их логику продумайте сами
+    }
+
+    public static int getInput()
+    {
+        System.out.println("Введите число: ");
+        return new Scanner(System.in).nextInt();
+    }
+
+    public static void printTrueForEven (int i)
+    {
+        System.out.println(i%2==0);
     }
 }
